b378b4620f6308990ae957fbf262b3ad9b074a0f
hbauhvagner <hbauhvagner@gmail.com>
2023-06-28 23:48:00 -0300

    Criando botão concluir

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -7,10 +7,12 @@ const criarTarefa = (evento) => {
 
     const tarefa = document.createElement('li');
     tarefa.classList.add('task');
-    
+
     const conteudo = `<p class="content">${valor}</p>`;
 
     tarefa.innerHTML = conteudo;
+
+    tarefa.appendChild(BotaoConclui());
     lista.appendChild(tarefa);
 
     input.value = '';
@@ -19,3 +21,16 @@ const criarTarefa = (evento) => {
 const novaTarefa = document.querySelector('[data-form-button]');
 
 novaTarefa.addEventListener('click', criarTarefa);
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button');
+
+    botaoConclui.classList.add('check-button');
+    botaoConclui.innerText = 'concluir';
+
+    botaoConclui.addEventListener('click', () => {
+        console.log('fui clicado');
+    });
+
+    return botaoConclui;
+}
