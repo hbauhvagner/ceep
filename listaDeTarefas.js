679887b60f1d296f355f07ec88f384ad9c69c486
hbauhvagner <hbauhvagner@gmail.com>
2023-06-28 23:24:29 -0300

    Adicionando Tarefa

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -3,7 +3,14 @@ const criarTarefa = (evento) => {
 
     const input = document.querySelector('[data-form-input]');
     const valor = input.value;
-    console.log(valor);
+
+    const tarefa = document.querySelector('[data-task]');
+
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+
+    input.value = '';
 }
 
 const novaTarefa = document.querySelector('[data-form-button]');
